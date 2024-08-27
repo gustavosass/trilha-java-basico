e8ef5cc90924695eb48eb2b166bd4d5fc6d76db4
Gustavo Sass <gustavosass@gmail.com>
2024-08-27 20:36:05 -0300

    add: collections
    Signed-off-by: Gustavo Sass <gustavosass@gmail.com>

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# Desafio POO - Iphone
+![alt text](https://github.com/gustavosass/trilha-java-basico/blob/main/DesafioPooIphoneUml/assets/Desafio-POO.drawio.png?raw=true)
---
+++ collections/br/com/exercicio/list/tarefa/Tarefa.java
@@ -0,0 +1,21 @@
+package br.com.exercicio.list.tarefa;
+
+public class Tarefa {
+	
+	private String descricao;
+	
+	public Tarefa(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+}
